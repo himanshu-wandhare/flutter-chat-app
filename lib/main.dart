d99cdade0df4c72859d19f8380fb4c3dca34b76f
Himanshu Wandhare <himanshuwandhare13@gmail.com>
2025-06-06 17:12:11 +0530

    setup Firebase SDK

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_chat_app/screens/auth.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await dotenv.load(fileName: ".env");
   runApp(const App());
 }
